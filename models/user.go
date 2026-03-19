7853167faf057142d4c90bc66f6e430224092269
adeleeeeyyyy <mitsuhaadly@gmail.com>
2026-03-19 16:22:00 +0700

    fix: Specify varchar length for google_id to avoid MySQL index error

--- models/user.go
+++ models/user.go
@@ -10,6 +10,6 @@ type User struct {
 	Email    string  `json:"email"                   gorm:"type:varchar(100);uniqueIndex;not null"`
 	Password *string `json:"-"                       gorm:"type:varchar(255)"`
 	Provider string  `json:"provider"                gorm:"type:varchar(50);not null;default:'manual'"`
-	GoogleID *string `json:"google_id,omitempty"     gorm:"uniqueIndex"`
+	GoogleID *string `json:"google_id,omitempty"     gorm:"type:varchar(255);uniqueIndex"`
 	Avatar   *string `json:"avatar,omitempty"        gorm:"type:varchar(255)"`
 }
